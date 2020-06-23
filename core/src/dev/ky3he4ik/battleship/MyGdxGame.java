089809ab6a4f5c055edcbdc3606017a6c4dbce8d
ky3he4ik <ky3he4ik@ky3he4ik>
2020-06-23 21:51:01 +0500

    Слияние MyGdxGame и MainView в один класс

--- core/src/dev/ky3he4ik/battleship/MyGdxGame.java
+++ core/src/dev/ky3he4ik/battleship/MyGdxGame.java
@@ -4,15 +4,26 @@ import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
 
 import org.jetbrains.annotations.NotNull;
 
-import dev.ky3he4ik.battleship.gui.MainView;
+import dev.ky3he4ik.battleship.gui.game_steps.StepsDirector;
 import dev.ky3he4ik.battleship.logic.StaticContent;
 import dev.ky3he4ik.battleship.platform.PlatformSpecific;
 import dev.ky3he4ik.battleship.utils.Constants;
 
-public class MyGdxGame extends Game {
+/**
+ * Класс игры и экрана. В конструктор передается платформо-зависимые API.
+ * Из логики только запуск музыки на повторе
+ * todo: перенести музыку в StepsDirector
+ */
+public class MyGdxGame extends Game implements Screen {
+    private StepsDirector stage;
+    private Music music;
+
     public MyGdxGame(@NotNull final PlatformSpecific platformSpecific) {
         StaticContent.createInstance(platformSpecific);
     }
@@ -21,6 +32,51 @@ public class MyGdxGame extends Game {
     public void create() {
         Gdx.input.setCatchKey(Input.Keys.BACK, true);
         Gdx.app.setLogLevel(Constants.DEBUG_MODE ? Application.LOG_DEBUG : Application.LOG_ERROR);
-        setScreen(new MainView());
+        stage = new StepsDirector();
+        Gdx.input.setInputProcessor(stage);
+        music = Gdx.audio.newMusic(Gdx.files.internal("TRG_Banks_-_Grandpas_great_escape.ogg"));
+        music.setLooping(true);
+        music.play();
+        music.setVolume(.4f);
+
+        setScreen(this);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.resize(width, height);
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+        music.dispose();
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void render(float delta) {
+        stage.act(delta);
+
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.draw();
+    }
+
+    @Override
+    public void pause() {
+        Gdx.graphics.setContinuousRendering(false);
+    }
+
+    @Override
+    public void resume() {
+        Gdx.graphics.setContinuousRendering(true);
+    }
+
+    @Override
+    public void hide() {
     }
 }
